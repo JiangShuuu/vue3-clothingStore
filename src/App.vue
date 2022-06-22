879b8eec33438c686b5ca8f6d1d8ee87c6e09fee
JiangShuuu <globelex65@gmail.com>
2022-06-22 20:32:24 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="h-[2000px]">
+    <div class="max-w-[1280px] mx-auto mt-1 flex flex-col justify-center">
+      <router-view></router-view>
+    </div>
+  </main>
+</template>
+
+<script setup>
+</script>
