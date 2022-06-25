61e5857ed44a7582d14f4da7d688b755a814e06f
JiangShuuu <globelex65@gmail.com>
2022-06-25 16:01:18 +0800

    add go top

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <Navbar />
-    <main class="h-[2000px]">
-      <div class="max-w-[1280px] mx-auto mt-1 flex flex-col justify-center">
-        <router-view></router-view>
-      </div>
-    </main>
+
+  <GoTop />
+  <main class="h-[2000px]">
+    <div class="max-w-[1280px] mx-auto mt-1 flex flex-col justify-center">
+      <router-view></router-view>
+    </div>
+  </main>
 </template>
 
 <script setup>
 import Navbar from './layouts/Navbar.vue'
+import GoTop from './layouts/GoTop.vue'
 
 </script>
