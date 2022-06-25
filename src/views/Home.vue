bf82e83b21b75e10eb54ddea931c4eedf7c7caee
JiangShuuu <globelex65@gmail.com>
2022-06-25 21:57:32 +0800

    add subscribe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full pt-12 md:pt-20">
+  <main class="w-full pt-12 md:pt-20">
     <Banner />
     <div class="flex-col w-full p-2 my-2 bg-gray-200 flex-center">
       <span class="text-xl font-light">HIGH QUALITY ASSURANCE</span>
@@ -7,17 +7,17 @@
       <span class="text-sm font-extralight">每一件商品都經過嚴格篩選</span>
       <span class="text-sm font-extralight">- 力求時尚兼具好品質 -</span>
     </div>
-    <div>
-      <Menu />
-      <ShopNow />
-    </div>
-  </div>
+    <Menu />
+    <ShopNow />
+    <Subscribe />
+  </main>
 </template>
 
 <script setup>
 import Banner from '../components/indexPage/Banner.vue'
 import Menu from '../components/indexPage/Menu.vue'
 import ShopNow from '../components/indexPage/ShopNow.vue'
+import Subscribe from '../components/indexPage/Subscribe.vue'
 </script>
 
 <style lang="postcss" scoped>
