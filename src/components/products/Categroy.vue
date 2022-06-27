a7e1c8405620f1f2f83046cfedbff3f8be978ab3
JiangShuuu <globelex65@gmail.com>
2022-06-27 21:36:22 +0800

    add sort

--- src/components/products/Categroy.vue
+++ src/components/products/Categroy.vue
@@ -1,17 +1,18 @@
 <template>
-  <main class="items-start space-y-5">
-    <div class="item">所有商品</div>
+  <main class="items-start pt-4 space-y-5">
+    <div class="item">全部商品</div>
     <div class="item">新上市</div>
-    <div class="item">特價商品</div>
+    <div class="item">熱賣商品</div>
     <div class="item">熱銷上衣</div>
     <div class="item">精選外套</div>
     <div class="item">經典褲款</div>
+    <div class="item">特價商品</div>
   </main>
 </template>
 <script setup>
 </script>
 <style lang="postcss" scoped>
 .item {
-  @apply cursor-pointer hover:text-primary;
+  @apply cursor-pointer text-lg hover:text-primary;
 }
 </style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -4,11 +4,17 @@
     <section class="flex-col justify-start w-1/5 flex-center">
       <Categroy />
     </section>
-    <section class="flex-1 border">456</section>
+    <section class="flex-1">
+      <div class="justify-between flex-center">
+        <span class="text-xl">全部商品</span>
+        <Sort />
+      </div>
+    </section>
   </main>
 </template>
 
 <script setup>
 import NavTab from '../components/global/NavTab.vue'
 import Categroy from '../components/products/categroy.vue'
+import Sort from '../components/products/Sort.vue'
 </script>
